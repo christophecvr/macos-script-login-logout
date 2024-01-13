58fde6f1d433048c5e398c04256d5132a1607034
Christophe Van Reusel <christophe@macbookprocvr-2.local>
2024-01-13 13:41:23 +0100

    Added the two script files to git repo
    Changes to be committed:
    	new file:   login-logout-script.plist
    	new file:   login-logout.sh

---
+++ login-logout.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Author christophecvr christophevanr@gmail.com
+# Based on boot-shutdown script of:
+# Vincenzo D'Amore v.damore@gmail.com
+# Adapted to work as user Launchagent.
+# 2024/01/12
+
+trap userlogout SIGTERM
+
+function userlogout()
+{
+	echo `date "+%Y-%m-%d %H:%M:%S"` "$USER" "Received a logout signal"
+
+	# INSERT HERE THE COMMAND OR SCRIPT YOU WANT EXECUTE AT USER LOGOUT
+
+	exit 0
+}
+
+function userlogin()
+{
+	echo `date "+%Y-%m-%d %H:%M:%S"` `whoami` "Logged in"
+
+	# INSERT HERE THE COMMAND OR SCRIPT YOU WANT EXECUTE AT USER LOGIN
+
+	tail -f /dev/null &
+	wait $!
+}
+
+userlogin;
