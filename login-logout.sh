c70d2b93e29919c24ffb31ae2c53c976aa97c0f5
Christophe Van Reusel <christophe@macbookprocvr-2.local>
2024-01-16 14:03:52 +0100

    Added a script file to excute at logout.
     On branch experimental
     Your branch is up to date with 'origin/experimental'.
     Changes to be committed:
    	modified:   login-logout.sh

--- login-logout.sh
+++ login-logout.sh
@@ -10,21 +10,26 @@ trap userlogout SIGTERM
 
 function userlogout()
 {
-	echo `date "+%Y-%m-%d %H:%M:%S"` "$USER" "Received a logout signal"
+  echo `date "+%Y-%m-%d %H:%M:%S"` "$USER" "Received a logout signal"
 
-	# INSERT HERE THE COMMAND OR SCRIPT YOU WANT EXECUTE AT USER LOGOUT
+  # INSERT HERE THE COMMAND OR SCRIPT YOU WANT EXECUTE AT USER LOGOUT
+  if [ -f ~/Library/Scripts/WisLibreofficeHistory.sh ];then
+    ~/Library/Scripts/WisLibreofficeHistory.sh
+  else
+    echo `date "+%Y-%m-%d %H:%M:%S"` "$USER" "Could not find Scriptfile WisLibreofficeHistory.sh"
+  fi
 
-	exit 0
+  exit 0
 }
 
 function userlogin()
 {
-	echo `date "+%Y-%m-%d %H:%M:%S"` `whoami` "Logged in"
+  echo `date "+%Y-%m-%d %H:%M:%S"` `whoami` "Logged in"
 
-	# INSERT HERE THE COMMAND OR SCRIPT YOU WANT EXECUTE AT USER LOGIN
+  # INSERT HERE THE COMMAND OR SCRIPT YOU WANT EXECUTE AT USER LOGIN
 
-	tail -f /dev/null &
-	wait $!
+  tail -f /dev/null &
+  wait $!
 }
 
 userlogin;
