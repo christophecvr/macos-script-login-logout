ab19162ab01a0c9650049be69e55986bcd9f1c63
Christophe Van Reusel <christophe@macbookprocvr-2.local>
2024-01-18 11:53:07 +0100

    Adapted the install.sh exit routine.
     This scriped is intented to run sourced means :
     start source install.sh or . install.sh .
     It can also run in a subshel means :
     start ./install.sh or sh install.sh .
     Since determining if you're running sourced or in a subshell
     Can change depending of the bash you're using.
     Using return 2> /dev/null ; exit should work always and killing script clean.
     for sh(dash),ksh,zsh,bash or ...
    
     On branch master
     Changes to be committed:
    	modified:   install.sh

--- install.sh
+++ install.sh
@@ -31,11 +31,7 @@ if [ "$SERVICE" == "loaded" ];then
   echo "The service is loaded but I can't find the installation"
   echo "I'm unable to remove it clean"
   echo "Aborting the script nothing is done"
-  if [ "$0" == "-bash" ];then
-    return
-  else
-    exit
-  fi
+  return 2> /dev/null; exit
 fi
 if [ "$SERVICE" == "loadedinstalled" ] || [ "$SERVICE" == "installed" ];then
   echo ""
@@ -47,11 +43,7 @@ if [ "$SERVICE" == "loadedinstalled" ] || [ "$SERVICE" == "installed" ];then
   read USER_ANSWER
   if [ "$USER_ANSWER" != "Yes" ];then
     echo "You choosed to abort the process"
-    if [ "$0" == "-bash" ];then
-      return
-    else
-      exit
-    fi
+    return 2> /dev/null; exit
   fi
 
   if [ "$SERVICE" == "loadedinstalled" ];then
@@ -94,11 +86,7 @@ if [ "$SERVICE" == "loadedinstalled" ] || [ "$SERVICE" == "installed" ];then
       echo "Git repo remains on pc"
       echo
     fi
-    if [ "$0" == "-bash" ];then
-      return
-    else
-      exit
-    fi
+    return 2> /dev/null; exit
   fi
 fi
 
